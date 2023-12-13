b79f119cc2fdf65885af3fced1042789dded4e52
iamannauser <anna.lushchan@gmail.com>
2023-12-13 22:25:36 +0200

    triangle homework

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        System.out.println(text);
+
+
+    }
+
+
+
+
+
+}
