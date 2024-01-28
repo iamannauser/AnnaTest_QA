36fd28c85c27ca65dac15fcbad2915fd567ffe8c
iamannauser <anna.lushchan@gmail.com>
2024-01-28 21:42:41 +0200

    Fibonacci - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55

---
+++ src/main/java/Test100.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class Test100 {
+
+ @Test
+    public void Translation(){
+     open("https://translate.google.com/");
+ }
+
+
+}
